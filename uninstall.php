53471098b370412bd4775a98df5c9805b03e1dd0
0jscsshtml <80338568+0jscsshtml@users.noreply.github.com>
2024-11-11 00:53:26 +0800

    Add files via upload

---
+++ uninstall.php
@@ -0,0 +1,18 @@
+<?php
+// uninstall.php
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}
+
+$all_users = get_users( array( 'fields' => 'ID' ) );
+foreach ( $all_users as $user_id ) {
+	delete_user_meta( intval($user_id), 'bc_user_current_login' );
+	delete_user_meta( intval($user_id), 'bc_user_last_login' );
+}
+	
+$cache_key_core = 'cached_' . md5('brickscodes' . '_bc_core_integration');
+delete_transient($cache_key_core);	
+$cache_key = 'cached_' . md5('brickscodes' . '_bc_query_manager');
+delete_transient($cache_key);
+delete_option('brickscodes');
